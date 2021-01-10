83a4e326b57c8a73679038efc47ace42e4192543
Mário Allan Lehmkuhl de Abreu <marioallan.la@gmail.com>
2021-01-10 02:25:03 -0300

    Add files

---
+++ Revisao/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var IndiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: Adcionar alunos
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota tem que ser decimal");
+                        }
+                            alunos[IndiceAluno] = aluno;
+                            IndiceAluno++;
+                        break;
+                    case "2":
+                        //TODO: Listar alunos
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                        }
+                        break;
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1- Insira novo do aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+
+            Console.WriteLine();
+            Console.Write("Informe: ");
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
