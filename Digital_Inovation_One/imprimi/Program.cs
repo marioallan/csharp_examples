d93f447051eb2278ae4c8789c208ab34f72801b7
Mário Allan Lehmkuhl de Abreu <marioallan.la@gmail.com>
2021-01-11 21:48:38 -0300

    Add files

---
+++ Digital_Inovation_One/imprimi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace imprimi
+{
+    class Dog
+    {
+        static void Main(string[] args)
+        {
+            
+            Dog a = new Dog();
+            Dog b = new Dog();
+
+            bool x = (a == b);
+            bool y = a.Equals(b);
+
+            //Console.WriteLine("x = " + c );
+        
+            //Console.WriteLine("x = " + x + ", y = " + y + ", z = " + z);
+            Console.WriteLine("x = " + x + ", y = " + y );
+            //Console.WriteLine("y = " + y );
+            //Console.WriteLine("x = " + x y;
+             //Console.WriteLine("x = " + c );
+            //Console.WriteLine("x = " + x.ToString("X4"));
+        
+        }
+    }
+}
